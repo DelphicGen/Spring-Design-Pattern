b6d10b2211405669c9c14c03f8e584e42a24be38
DelphicGen <gennardokidam@gmail.com>
2021-01-28 11:30:29 +0700

    Bean names

--- src/main/java/com/blibli/belajar/design/pattern/protype/PrototypeApplication.java
+++ src/main/java/com/blibli/belajar/design/pattern/protype/PrototypeApplication.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -26,12 +27,18 @@ public class PrototypeApplication {
     @SpringBootApplication
     public static class Configuration {
 
-        @Bean
+        // kalau mw custom nama bean
+        @Bean("standardCustomer")
         @Scope("prototype")
         public Customer standardCustomer() {
             return Customer.builder().category("STANDARD").discount(10).build();
         }
 
+        @Bean
+        @Scope("prototype")
+        public Customer premiumCustomer() {
+            return Customer.builder().category("PREMIUM").discount(50).build();
+        }
     }
 
     public static void main(String[] args) {
@@ -39,15 +46,25 @@ public class PrototypeApplication {
 //        Customer customerStandard2 = Customer.builder().id("1").name("Eko").category(customerStandard1.getCategory()).discount(customerStandard1.getDiscount()).build();
 //        Customer customerStandard3 = Customer.builder().id("1").name("Eko").category(customerStandard1.getCategory()).discount(customerStandard1.getDiscount()).build();
 //        Customer customerStandard4 = Customer.builder().id("1").name("Eko").category(customerStandard1.getCategory()).discount(customerStandard1.getDiscount()).build();
+//        Customer customerStandard5 = new Customer();
+//        BeanUtils.copyProperties(customerStandard1, customerStandard5); // tapi harus tau lokasi kedua posisi / lokasi object
 
         ApplicationContext context = SpringApplication.run(Configuration.class);
-        Customer customerStandard1 = context.getBean(Customer.class);
-        Customer customerStandard2 = context.getBean(Customer.class);
-        Customer customerStandard3 = context.getBean(Customer.class);
+        Customer customerStandard1 = context.getBean("standardCustomer", Customer.class);
+        Customer customerStandard2 = context.getBean("standardCustomer", Customer.class);
+        Customer customerStandard3 = context.getBean("standardCustomer", Customer.class);
+
+        Customer customerPremium1 = context.getBean("premiumCustomer", Customer.class);
+        Customer customerPremium2 = context.getBean("premiumCustomer", Customer.class);
+        Customer customerPremium3 = context.getBean("premiumCustomer", Customer.class);
 
         System.out.println(customerStandard1 == customerStandard2);
         System.out.println(customerStandard1);
         System.out.println(customerStandard2);
         System.out.println(customerStandard3);
+
+        System.out.println(customerPremium1);
+        System.out.println(customerPremium2);
+        System.out.println(customerPremium3);
     }
 }

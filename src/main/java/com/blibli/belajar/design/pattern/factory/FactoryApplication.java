dea00d1f025993d4109c58f1e366844ad86cd811
DelphicGen <gennardokidam@gmail.com>
2021-01-28 10:47:32 +0700

    Factory test

---
+++ src/main/java/com/blibli/belajar/design/pattern/factory/FactoryApplication.java
@@ -0,0 +1,58 @@
+package com.blibli.belajar.design.pattern.factory;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class FactoryApplication {
+
+    @Data
+    @AllArgsConstructor
+    public static class Database {
+
+        private String host;
+        private Integer port;
+        private String username;
+        private String password;
+    }
+
+    public static class DatabaseFactory implements FactoryBean<Database> {
+
+        @Override
+        public Database getObject() throws Exception {
+            Database database = new Database("localhost", 1000, "admin", "admin");
+            return database;
+        }
+
+        @Override
+        public Class<?> getObjectType() {
+            return null;
+        }
+    }
+
+    @SpringBootApplication
+    public static class Configuration {
+
+//        @Bean // harus masukkin byk param
+//        public Database database() {
+//            Database database = new Database();
+//            return database;
+//        }
+
+        @Bean
+        public FactoryBean<Database> database() {
+            return new DatabaseFactory();
+        }
+
+        public static void main(String[] args) {
+            ApplicationContext context = SpringApplication.run(Configuration.class);
+            Database database = context.getBean(Database.class);
+        }
+
+    }
+}
--- src/main/java/com/blibli/belajar/design/pattern/singleton/SingletonApplication.java
+++ src/main/java/com/blibli/belajar/design/pattern/singleton/SingletonApplication.java
@@ -8,19 +8,29 @@ import org.springframework.context.annotation.Bean;
 public class SingletonApplication {
     @SpringBootApplication
     public static class Configuration {
-        @Bean
+        @Bean // singleton denga tipe data Contoh, dan manggil emthod nya
         public Contoh contoh() {
             return new Contoh();
         }
     }
 
     public static class Contoh {
-
     }
+
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SingletonApplication.Configuration.class);
-        Contoh contoh1 = context.getBean(SingletonApplication.Contoh.class);
-        Contoh contoh2 = context.getBean(SingletonApplication.Contoh.class);
+        Contoh contoh1 = context.getBean(Contoh.class);
+        Contoh contoh2 = context.getBean(Contoh.class);
+
+//        Kalau manual, bikin controller perlu service hrs set satu"
+//        A a = new A();
+//        B b = new B();
+//        b.setA(a);
+//        b.setC(c);
+//        b.setD(d);
+//        Kalau pake spring akan diinject otomatis a ke b
+//        A a = context.getBena(A.class);
+//        B b = context.getBean(B.class); saat B.class semua dependencynya bakal di inject oleh spring
 
 //        Kalau bikin objectnya manual (new Contoh), pasti false karena dua object yg berbeda
         System.out.println(contoh1 == contoh2);
